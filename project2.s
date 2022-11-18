d7b3cc738183621e6391cd4f8c08f75dcab24f0b
stew-CSO <113641083+stew-CSO@users.noreply.github.com>
2022-11-18 12:59:19 -0500

    Update project2.s
    moved register $a0 to register $t0 
    input: .space 1001 will read up to 1000 characters
    take the contents of memory, load it, and sign extend the result to 32 (or 64) bits

--- project2.s
+++ project2.s
@@ -7,7 +7,7 @@
 .data ##data section
 
 result: .asciiz "Unrecognized Input" 
-input: .space 1000
+input: .space 1001
 
 .text
 	.globl main
@@ -15,6 +15,10 @@ input: .space 1000
 main: # Start of code section
 
 li $v0, 8
-la $a0, result 
-li $a1, 1000
+la $a0, user_input 
+li $a1, 1001
 syscall 
+
+move $t0, $a0  #move userInput into register $t0 
+lbu $s0, ($t0) #take the contents of memory, load it, and sign extend the result to 32 (or 64) bits.
+
