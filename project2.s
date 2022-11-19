3838ca88603c68daf20991b0c09d6ceb17fe0b9a
stew-CSO <113641083+stew-CSO@users.noreply.github.com>
2022-11-19 01:24:00 -0500

    Update project2.s
    removed unnecessary code
    
    starting to create the begin loop

--- project2.s
+++ project2.s
@@ -24,75 +24,69 @@ li $a1, 1001
 syscall 
 
 move $s0, $a0  #move userInput into register $s0 
-
-
-
-lb $t0, 0($s0)
-beq $t0, 10, unrecognize
-
-
-lb $t0, 5($s0)
-jal Check_Character
-
+add $t1, $s0, $zero
+addi $t0, $t0, 1001
 
 
 
 lb $t0, 3($s0)
-jal check
-lb $t0, 2($s0)
-jal check
-lb $t0, 1($s0)
-jal check
-lb $t0, 0($s0)
-jal check
+
+
+
+
+
+
+
 
 bgt $t2, 4, unrecognize 
 j exitfunction	
 
 
+
+
 outOfBounds:
-j unrecognize 
+	j unrecognize 
 
 
 Check_Character:
-beq $t0, 10, unrecognize
-bne $t0, $zero, unrecognize
-jr $ra
+	beq $t0, 10, unrecognize
+	bne $t0, $zero, unrecognize
+	jr $ra
 
 check:
-bgt $t0, 0, add_Character
-jr $ra
+	bgt $t0, 0, add_Character
+	jr $ra
 
 
 
 
 
 add_Character:
-bgt $t0, 96, lowerCaseChar
-bgt $t0, 64, upperCaseChar
-bgt $t0, 47, num
-bne $t0, 10, outOfBounds
-jr $ra
+	bgt $t0, 96, lowerCaseChar
+	bgt $t0, 64, upperCaseChar
+	bgt $t0, 47, num
+	bne $t0, 10, outOfBounds
+	jr $ra
 
 
 
 #convert base number to decimal
 base28:
-beq $t2, $t3, addsLoop
-multu $t0, $s5
-mflo $t4
-mfhi $t5
-add $t0, $t4, $t5
-addi $t2, 1
-bne $t2, $t3, base28
+	beq $t2, $t3, addsLoop
+	multu $t0, $s5
+	mflo $t4
+	mfhi $t5
+	add $t0, $t4, $t5
+	addi $t2, 1
+	bne $t2, $t3, base28
 	
 
 
 addsLoop:
-sub $t2, $t2, $t2
-add $t9, $t0, $t9
-addi $t3, 1
-jr $ra
+	sub $t2, $t2, $t2
+	add $t9, $t0, $t9
+	addi $t3, 1
+	jr $ra
 
 
 
@@ -121,42 +115,32 @@ exitfunction:
 #LowerCase loop 
 
 lowerCaseChar:
-bgt $t0, 114, outOfBounds
-sub $t0, $t0, 87
-j base28
+	bgt $t0, 114, outOfBounds
+	sub $t0, $t0, 87
+	j base28
  
 
 upperCaseChar: 
-bgt $t0, 82, outOfBounds 
-sub $t0, $t0, 55
-j base28
+	bgt $t0, 82, outOfBounds 
+	sub $t0, $t0, 55
+	j base28
 
 
 num:
-bgt $t0, 57, outOfBounds  
-sub $t0, $t0, 48
-j base28 
-
-
-
-
-#function to remove tab 
-tab:
-	addi $a0, $a0, 1
-	move $t0, $a0
-
-
+	bgt $t0, 57, outOfBounds  
+	sub $t0, $t0, 48
+	j base28 
 
 
 
 
 unrecognize: 
-la $a0, result
-li $v0, 4
-syscall
+	la $a0, result
+	li $v0, 4
+	syscall
 	
-li $v0, 10
-syscall
+	li $v0, 10
+	syscall
 
 
 
